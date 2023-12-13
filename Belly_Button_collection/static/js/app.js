d8d977753b1e3463e5f4f5d8461208101419dade
mwolfgangmath <140471930+mwolfgangmath@users.noreply.github.com>
2023-12-13 14:54:01 -0600

    hbar and bubble functions working

--- Belly_Button_collection/static/js/app.js
+++ Belly_Button_collection/static/js/app.js
@@ -1,39 +1,33 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Get the data directly from the URL
-function log_data(data){
-    console.log("Belly Button Diversity Data: ", data);
-}
-let navel_data = d3.json(url);
-log_data(navel_data)
+// Get the data directly from the URL and log to the console to view
 
-navel_data.then(log_data);
+
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
-  console.log(data);
+  console.log("Belly Button Diversity Data: ", data);
 });
 
-function optionChanged(subject_id){
-    console.log("Option Changed: ", subject_id);
-}
-
-function init(){
+//Initiate dashboard at start up.
+function init() {
     //populate the dropdown
     let drop_down = d3.select("#selDataset"); 
     d3.json(url).then(data => {
        let ids = data.names;
-       ids.forEach(id => drop_down.append("option").attr("value",id).text(id))
-       optionChanged(ids[0]);
+       console.log(ids);
+       ids.forEach((id) => drop_down.append("option").attr("value",id).text(id));
        //access first subject ID
        let og_sample = ids[0];
-
+       console.log(og_sample);
+       
        //initial analysis and plots for the first subject
+       ///////////////  optionChanged(ids[0]); ///////////////
        store_meta_data(og_sample);
        subject_hbar_chart(og_sample);
-       //bubbleChart(og_sample);
-      });    
+       subject_bubble_chart(og_sample);
+    });    
 };
-init();
+
 //Create a horizontal bar chart to display the top 10 OTUs found in that individual.
 
 //Retreive and display the individual subject's metadata: demographic information.   
@@ -43,7 +37,6 @@ function store_meta_data(subject) {
         let value = meta_data.filter(result => result.id == subject);
         console.log(value);
         let value_data = value[0];
-        //DEBUG: metadata is logging to console, but not displaying to the dashboard.
         d3.select("#sample-metadata").html("");
         Object.entries(value_data).forEach(([key,value]) => {
             console.log(key,value);
@@ -53,29 +46,68 @@ function store_meta_data(subject) {
 };
 
 function subject_hbar_chart(subject){
-d3.json(url).then((data) => {
-    let value = data.samples.filter(result => result.id == subject);
-    let value_data = value[0];
-    // Create an array of category labels
-    let labels = value_data.otu_labels.slice(0,10).reverse();
+    d3.json(url).then((data) => {
+        let value = data.samples.filter(result => result.id == subject);
+        let value_data = value[0];
+        
+        let sample_values = value_data.sample_values.slice(0,10).reverse();
+        let otu_ids = value_data.otu_ids.slice(0,10).reverse().map((id) => `OTU ${id}`);
+        let otu_labels = value_data.otu_labels.slice(0,10).reverse();
 
-//slice the TopTen, then reverse order for horizontal bar chart
+        console.log(otu_ids, otu_labels, sample_values)
+
     let trace1 = {
-        x: value_data.sample_values.slice(0,10).reverse(),
-        y: value_data.otu_ids.slice(0,10).reverse(),
-        text: labels,
+        x: sample_values,
+        y: otu_ids,
+        labels: otu_labels,
         type: "bar",
-        orientation: "h"
+        orientation: "h",
     };
-    
     let traceData = [trace1];
 
     let layout = {
         title: `Top Ten OTU's for subject #${subject}`
+        //height: 500,
+        //width:600,
         };
+        
     Plotly.newPlot("bar", traceData, layout)
     });
 };
+
+function subject_bubble_chart(subject){
+    d3.json(url).then((data) => {
+        let value = data.samples.filter(result => result.id == subject);
+        let value_data = value[0];
+        
+        let sample_values = value_data.sample_values;
+        let otu_ids = value_data.otu_ids;
+        let otu_labels = value_data.otu_labels;
+
+    let trace2 = {
+        x: otu_ids,
+        y: sample_values,
+        labels: otu_labels,
+        mode: "markers",
+        marker:{
+            size: sample_values,
+            color: otu_ids,
+            colorscale: "Earth"
+        }
+    };
+    let traceData = [trace2];
+    let layout = {
+        title: `Bacteria/Sample for Subject #${subject}`,
+        hovermode: "closest",
+        xaxis: {title:"OTU ID"},
+        };
+        
+    Plotly.newPlot("bubble", traceData, layout)
+    });
+};
+
+init();
+
 
   // Render the plot to the div tag with id "plot"
 
@@ -95,4 +127,3 @@ d3.json(url).then((data) => {
 
 //Deploy your app to a free static page hosting service, such as GitHub Pages. 
 //Submit the links to your deployment and your GitHub repo. 
-
